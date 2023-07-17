009c5893fca627e2c412d05cea9df1095d5c372e
Tilaknaik01 <129579015+Tilaknaik01@users.noreply.github.com>
2023-07-17 17:40:21 +0530

    Update ArraylistToArray.java

--- SDET48/src/test/java/pattern/ArraylistToArray.java
+++ SDET48/src/test/java/pattern/ArraylistToArray.java
@@ -3,7 +3,8 @@ package pattern;
 import java.util.ArrayList;
 
 public class ArraylistToArray {
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
+		//hbgbbgbgt			
 		ArrayList<Object> arrayList = new ArrayList<Object>();
 		arrayList.add("Tilak1");
 		arrayList.add(1);
@@ -11,7 +12,7 @@ public class ArraylistToArray {
 		arrayList.add("Tilak4");
 		Object[] array = arrayList.toArray();
 		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+System.out.println(array[i]);
 		}
 	}
 }
